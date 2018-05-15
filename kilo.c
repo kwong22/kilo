059762edcffc66537da7e7c1d1e540e4720b52f6
Kelvin Wong <kwong691@gmail.com>
2018-05-15 15:59:33 -0400

    Draw tildes to screen

--- kilo.c
+++ kilo.c
@@ -7,6 +7,12 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** defines ***/
+
+// macro that bitwise-ANDs a character with 00011111 (sets upper 3 bits to 0)
+// equivalent to Ctrl-character
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 /*** data ***/
 
 // store original terminal attributes
@@ -16,6 +22,10 @@ struct termios orig_termios;
 
 // handles error stored in global errno variable
 void die(const char *s) {
+  // clear screen and position cursor
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
   // prints s and descriptive message for errno
   perror(s);
 
@@ -73,27 +83,78 @@ void enableRawMode() {
     die("tcsetattr");
 }
 
+// waits for keypress and returns it
+// in terminal section because it deals with low-level terminal input
+char editorReadKey() {
+  int nread;
+  char c;
+
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    // when read() times out in Cygwin, it returns -1 and errno of EAGAIN
+    // don't treat EAGAIN as an error
+    if (nread == -1 && errno != EAGAIN)
+      die("read");
+  }
+
+  return c;
+}
+
+/*** input ***/
+
+// asks for keypress and handles it
+// in input section because it maps keys to functions at a higher level
+void editorProcessKeypress() {
+  char c = editorReadKey();
+
+  switch (c) {
+    // Ctrl-Q exits the program and clears screen
+    case CTRL_KEY('q'):
+      // clear screen and position cursor
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
+
+      exit(0);
+      break;
+  }
+}
+
+/*** output ***/
+
+// draws each row of the text buffer
+void editorDrawRows() {
+  int y;
+  for (y = 0; y < 24; y++) {
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
+}
+
+void editorRefreshScreen() {
+  // write 4 bytes to the terminal (using VT100 escape sequences):
+  // \x1b - escape character (27)
+  //    [ - after escape character to begin escape sequence
+  //    2 - clear entire screen, 1 up to cursor, 0 after cursor
+  //    J - clear screen
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+
+  // use H command to position cursor
+  // add x;y argument before H for x row and y column, indexing starts with 1
+  // 1;1 is default
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
+  editorDrawRows();
+
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
 /*** init ***/
 
 int main() {
   enableRawMode();
   
-  // read until q is entered
   while (1) {
-    char c = '\0';
-
-    // when read() times out in Cygwin, it returns -1 and errno of EAGAIN
-    // don't treat EAGAIN as an error
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-      die("read");
-
-    if (iscntrl(c)) { // checks if c is a control character, nonprintable
-      printf("%d\r\n", c); // %d to format byte as a decimal number
-    } else {
-      printf("%d ('%c')\r\n", c, c); // %c to write out byte as a character
-    }
-
-    if (c == 'q') break;
+    editorRefreshScreen();
+    editorProcessKeypress();
   }
+
   return 0;
 }
